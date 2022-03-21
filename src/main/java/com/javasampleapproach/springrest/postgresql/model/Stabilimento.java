93cc9da85fe33ab658048c407b136efc27b5355a
Artorias718 <lai.ndr94@gmail.com>
2022-03-21 18:43:14 +0100

    temporaneamente disabilitata l'autenticazione

--- src/main/java/com/javasampleapproach/springrest/postgresql/model/Stabilimento.java
+++ src/main/java/com/javasampleapproach/springrest/postgresql/model/Stabilimento.java
@@ -26,8 +26,8 @@ public class Stabilimento {
     public Long getId() {
         return id;
     }
-    public void setId(Long idd) {
-        this.id = idd;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {this.name = name;}
